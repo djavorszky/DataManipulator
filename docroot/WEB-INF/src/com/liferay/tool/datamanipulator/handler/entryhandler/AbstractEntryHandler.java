d3476aa259456b6eaa21f6514a783b520982d075
tiborkovacs <tibor.kovacs@liferay.com>
2014-01-14 13:51:06 +0100

    SourceFormat on Abstract*Handler

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/handler/entryhandler/AbstractEntryHandler.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/handler/entryhandler/AbstractEntryHandler.java
@@ -33,10 +33,10 @@ import com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor;
  *
  */
 public abstract class AbstractEntryHandler implements EntryHandlerModel {
+
 	public AbstractEntryHandler(
 		int count, int update, int depth, int subCount, BaseEntry baseEntry,
-		EntryHandlerModel subEntryHandler,
-		RequestProcessor requestProcessor) {
+		EntryHandlerModel subEntryHandler, RequestProcessor requestProcessor) {
 
 		_baseEntry = baseEntry;
 		_count = count;
@@ -77,6 +77,7 @@ public abstract class AbstractEntryHandler implements EntryHandlerModel {
 		throws PortalException, SystemException {
 
 		// TODO Auto-generated method stub
+
 	}
 
 	@Override
@@ -192,7 +193,7 @@ public abstract class AbstractEntryHandler implements EntryHandlerModel {
 
 			if (exceptionString.contains("Duplicate") ||
 				(Validator.isNotNull(exceptionMessage) &&
-				exceptionMessage.contains("already exists"))) {
+				 exceptionMessage.contains("already exists"))) {
 
 				return true;
 			}
@@ -214,9 +215,9 @@ public abstract class AbstractEntryHandler implements EntryHandlerModel {
 	private BaseEntry _baseEntry;
 	private int _count;
 	private int _depth;
-	private EntryHandlerModel _subEntryHandler;
 	private RequestProcessor _requestProcessor;
 	private int _subCount;
+	private EntryHandlerModel _subEntryHandler;
 	private int _update;
 
 }
