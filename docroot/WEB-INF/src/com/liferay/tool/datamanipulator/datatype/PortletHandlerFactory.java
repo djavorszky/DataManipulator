548fa1906a4c6a499b35bf16615123f1f0716146
Laszlo Csontos <laszlo.csontos@liferay.com>
2014-02-12 09:34:22 +0100

    Custom handler for generating vocabularies

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/PortletHandlerFactory.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/PortletHandlerFactory.java
@@ -20,6 +20,7 @@ import com.liferay.tool.datamanipulator.datatype.blogs.BlogsHandler;
 import com.liferay.tool.datamanipulator.datatype.bookmarks.BookmarksHandler;
 import com.liferay.tool.datamanipulator.datatype.calendar.CalendarHandler;
 import com.liferay.tool.datamanipulator.datatype.categories.CategoriesHandler;
+import com.liferay.tool.datamanipulator.datatype.custom.CustomVocabularyHandler;
 import com.liferay.tool.datamanipulator.datatype.documentsandmedia.DocumentsAndMediaHandler;
 import com.liferay.tool.datamanipulator.datatype.journal.JournalHandler;
 import com.liferay.tool.datamanipulator.datatype.layout.LayoutHandler;
@@ -91,6 +92,10 @@ public final class PortletHandlerFactory {
 
 		if (entryTypeKey.equals(EntryTypeKeys.PORTAL_USER)) {
 			return new UserHandler();
+		}
+
+		if (entryTypeKey.equals(EntryTypeKeys.CUSTOM_ENTRY_HANDLER)) {
+			return new CustomVocabularyHandler();
 		}
 
 		return null;
